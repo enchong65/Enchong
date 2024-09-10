309d3191c4470daedaabb9bd37e2cbc5103765e4
Timmyfish <Timmyfish@ruanshiyudeMacBook-Pro.local>
2024-09-10 15:56:29 +0800

    change database url

--- config.py
+++ config.py
@@ -8,5 +8,5 @@ class DevConfig(Config):
     SQLALCHEMY_DATABASE_URI = 'postgresql://admin:Timmy055055@localhost:5432/Enchong'
 
 class ProdConfig(Config):
-    DEBUG = False
+    DEBUG = True
     SQLALCHEMY_DATABASE_URI = os.environ.get('postgresql://database_rzcc_user:vzHmHnYl7XI27VZ52vyHcyHgoKLZ6iT6@dpg-crfuoabqf0us73dd198g-a/database_rzcc')
